c48b0a6d98237b325e853ba58d24428c18242dcb
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-01-21 18:57:14 +0900

    feat: Create 기능 구현(제출버튼  클릭 시 화면에 추가)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,26 @@
+import { useState } from "react";
+
 function App() {
+  const [addRegion, setAddRegion] = useState({
+    region: "",
+    goldMedal: 0,
+    silverMedal: 0,
+    bronzeMedal: 0,
+  });
+
+  const handleChange = (e) => {
+    setAddRegion({
+      ...addRegion,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const [region, setRegions] = useState([]);
+  const handleForm = (e) => {
+    e.preventDefault();
+    setRegions([...region, addRegion]);
+  };
+
   return (
     <div className="main">
       <div className="container">
@@ -6,39 +28,70 @@ function App() {
           <h1>2024 파리 올림픽</h1>
         </div>
         <div className="form">
-          <form action="">
+          <form action="#" onSubmit={handleForm}>
             <table>
-              <tr>
-                <th>국가명</th>
-                <th>금메달</th>
-                <th>은메달</th>
-                <th>동메달</th>
-                <th></th>
-                <th></th>
-              </tr>
-              <tr>
-                <td>
-                  <input type="text" placeholder="국가 입력" autoFocus />
-                </td>
-                <td>
-                  <input type="number" name="" id="" value={0} />
-                </td>
-                <td>
-                  <input type="number" name="" id="" value={0} />
-                </td>
-                <td>
-                  <input type="number" name="" id="" value={0} />
-                </td>
-                <td>
-                  <button>국가 추가</button>
-                </td>
-                <td>
-                  <button>업데이트</button>
-                </td>
-              </tr>
+              <thead>
+                <tr>
+                  <th>국가명</th>
+                  <th>금메달</th>
+                  <th>은메달</th>
+                  <th>동메달</th>
+                  <th></th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <input
+                      type="text"
+                      name="region"
+                      placeholder="국가 입력"
+                      value={addRegion.region || ""}
+                      onChange={handleChange}
+                      autoFocus
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="number"
+                      name="goldMedal"
+                      value={addRegion.goldMedal || 0}
+                      min="0"
+                      onChange={handleChange}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="number"
+                      name="silverMedal"
+                      value={addRegion.silverMedal || 0}
+                      min="0"
+                      onChange={handleChange}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="number"
+                      name="bronzeMedal"
+                      value={addRegion.bronzeMedal || 0}
+                      min="0"
+                      onChange={handleChange}
+                    />
+                  </td>
+                  <td>
+                    <button type="submit">국가 추가</button>
+                  </td>
+                  <td>
+                    <button type="submit">업데이트</button>
+                  </td>
+                </tr>
+              </tbody>
             </table>
-            <p>아직 추가된 국가가 없습니다. 메달을 추적하세요!</p>
           </form>
+          <RegionList region={region}>
+            {/* 아직 추가된 국가가 없습니다. 메달을 추적하세요! */}
+          </RegionList>
         </div>
       </div>
     </div>
@@ -46,3 +99,40 @@ function App() {
 }
 
 export default App;
+
+const RegionList = (p) => {
+  return (
+    <table className="region-list">
+      <thead>
+        <tr>
+          <th>국가명</th>
+          <th>금메달</th>
+          <th>은메달</th>
+          <th>동메달</th>
+          <th>액션</th>
+        </tr>
+      </thead>
+      <tbody>
+        <AddRegion region={p.region} />
+      </tbody>
+    </table>
+  );
+};
+
+const AddRegion = (p) => {
+  return (
+    <>
+      {p.region.map((x, index) => (
+        <tr key={index}>
+          <td>{x.region}</td>
+          <td>{x.goldMedal}</td>
+          <td>{x.silverMedal}</td>
+          <td>{x.bronzeMedal}</td>
+          <td>
+            <button>삭제</button>
+          </td>
+        </tr>
+      ))}
+    </>
+  );
+};
